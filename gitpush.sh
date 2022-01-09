fe709e966cf26778477e7e7216037053a7a729a7
remy crochon <crochonremy@gmail.com>
2022-01-10 00:29:12 +0100

    config files on 10-01-2022 00:29:12

--- gitpush.sh
+++ gitpush.sh
@@ -1,12 +1,24 @@
 # Allez dans le dossier /config ou
 # Remplacez-le par votre dossier de configuration Home Assistant s'il est différent
-cd /config
+##cd /config
 
 # Ajouter tous les fichiers au référentiel en respectant les règles .gitignore
-git add .
+##git add .
 
 # Valider les modifications avec le message de l'horodatage actuel
-git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+##git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
 
 # Pousser les changements vers GitHub
+##git push -u origin master
+
+
+#!/bin/bash
+date >> /config/git-last.txt
+cd /config
+git add .
+git status
+# Commit changes with message with current date stamp
+git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+# Push changes towards GitHub
 git push -u origin master
+exit

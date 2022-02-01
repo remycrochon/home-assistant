8f7082b9f558fd1540c01591d7ed3b9297e1235f
root <root@homeassistant.local.hass.io>
2022-02-01 02:00:01 +0100

    config files on 01-02-2022 02:00:01

--- appdaemon/apps/groupe_alerte.py
+++ appdaemon/apps/groupe_alerte.py
@@ -19,22 +19,28 @@ class GroupeAlerte(hass.Hass):
 
     def hors_reseau(self, entity, attribute, old, new, kwargs):
         heure = str(self.time())[:8]
-        timer_key = str(entity)
+        timer_key_off = str(entity)
+        timer_key_on = str(entity)
         friendly_name = self.friendly_name(entity)
         if new == "off":
-            self.log(str(entity) + ' est deconnectee ',log="diag_log")
+            self.log(f'{friendly_name} est deconnectee ',log="diag_log")
             self.log(f'Debut tempo pour {friendly_name}')
-            self.timers[timer_key] = self.run_in(self.send_notification, 60, entité_n= friendly_name, timer= timer_key )
+            self.timers[timer_key_off] = self.run_in(self.send_notification_off, 60, entité_n= friendly_name, timer= timer_key_off )
+            timer_on = self.timers[timer_key_on]
+            if timer_on != None:
+                self.log(f'Fin tempo off pour {friendly_name}')
+                self.timers[timer_key_on] = self.cancel_timer(timer_on) 
         else:
 #            self.log(str(friendly_name) + ' est connectee',log="diag_log")
 #            self.call_service('notify/telegram', message=format(heure)+" : Entité <" +format(friendly_name)+" > connectée!")
 #            self.call_service('persistent_notification/create', message=format(heure)+" : Entité <" +format(friendly_name)+" > connectée!")
-            timer = self.timers[timer_key]
-            if timer != None:
-                self.log(f'Fin tempo pour {friendly_name}')
-                self.timers[timer_key] = self.cancel_timer(timer) 
+            timer_off = self.timers[timer_key_off]
+            if timer_off != None:
+                self.log(f'Fin tempo on pour {friendly_name}')
+                self.timers[timer_key_off] = self.cancel_timer(timer_off) 
+            self.timers[timer_key_off] = self.run_in(self.send_notification_on, 60, entité_n= friendly_name, timer= timer_key_off )
 
-    def send_notification(self, kwargs):
+    def send_notification_off(self, kwargs):
         heure = str(self.time())[:8]
         entité_nom = kwargs["entité_n"]
         timer_key = kwargs["timer"]
@@ -44,3 +50,14 @@ class GroupeAlerte(hass.Hass):
         self.log(f'Alerte! {entité_nom} est deconnecte depuis 1 mn.') 
         self.call_service('notify/telegram', message=format(heure)+": Entité <" +format(entité_nom)+" > est deconnectée depuis 1mn!")
         self.call_service('persistent_notification/create', message=format(heure)+" : Entité <" +format(entité_nom)+" > est deconnectée depuis 1mn!")
+
+    def send_notification_on(self, kwargs):
+        heure = str(self.time())[:8]
+        entité_nom = kwargs["entité_n"]
+        timer_key = kwargs["timer"]
+        # forget timer reference
+        self.timers[timer_key] = None
+        # send your notification   
+        self.log(f'Alerte! {entité_nom} est connecte depuis 1 mn.') 
+        self.call_service('notify/telegram', message=format(heure)+": Entité <" +format(entité_nom)+" > est connectée depuis 1mn!")
+        self.call_service('persistent_notification/create', message=format(heure)+" : Entité <" +format(entité_nom)+" > est connectée depuis 1mn!")        

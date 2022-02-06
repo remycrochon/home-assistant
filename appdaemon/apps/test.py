647addd7f7ad1e272f882f5fb3f35f5ba32d9f8e
root <root@homeassistant.local.hass.io>
2022-02-06 02:00:01 +0100

    config files on 06-02-2022 02:00:01

--- appdaemon/apps/test.py
+++ appdaemon/apps/test.py
@@ -4,37 +4,11 @@ from datetime import datetime, timedelta
 
 class Test(hass.Hass):
     def initialize(self):
-        self.listen_state(self.change,self.args["h1"])
-        self.listen_state(self.change,self.args["h2"])
-        self.listen_state(self.change_mode,self.args["mode_de_fonctionnement"])
-        self.log('Initialisation.......')
-        
-
+        self.my_enitity = self.get_entity("input_boolean.test_alert")
+        self.handle = self.my_enitity.listen_state(self.change)
+        self.log('Initialisation Test..', log="test_log")
 
     def change(self, entity, attribute, old, new, kwargs):
-        self.log(f'Test.')
-        h11=self.get_state(self.args["h1"]) #.strftime("%H:%M:%S") 
-        h_pivot_max="14:00:00"
-        h_pivot_min="11:00:00"
-        if h11>h_pivot_max:
-            h11=h_pivot_max
-            self.set_state(self.args["h1"], state = h_pivot_max)
-        if h11<h_pivot_min:
-            h11=h_pivot_min
-            self.set_state(self.args["h1"], state = h_pivot_min)
+        self.log(f'Etat:{new}', log="test_log")
+        pass
 
-        h22=self.get_state(self.args["h2"]) #.strftime("%H:%M:%S") 
-        self.log(f'h11= {h11}')
-        self.log(f'h22= {h22}')
-
-    def change_mode(self, entity, attribute, old, new, kwargs):
-        self.log(f'change mode.')
-        liste_mode=self.get_state(self.args["mode_de_fonctionnement"], attribute="options")
-        self.log(f'mode= {liste_mode[0]}')
-        self.log(f'mode= {liste_mode[1]}')
-        self.log(f'mode= {liste_mode[2]}')
-        self.log(f'mode= {liste_mode[3]}')
-        i=0
-        for i in range(liste_mode):
-            self.log(f'mode= {liste_mode[i]}')
-            

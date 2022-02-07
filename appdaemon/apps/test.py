899ab81551bc11c62364ba9d551b567454ebc48b
root <root@homeassistant.local.hass.io>
2022-02-07 02:00:01 +0100

    config files on 07-02-2022 02:00:00

--- appdaemon/apps/test.py
+++ appdaemon/apps/test.py
@@ -4,9 +4,11 @@ from datetime import datetime, timedelta
 
 class Test(hass.Hass):
     def initialize(self):
-        self.my_enitity = self.get_entity("input_boolean.test_alert")
-        self.handle = self.my_enitity.listen_state(self.change)
+        #self.my_enitity = self.get_entity("input_boolean.test_alert")
+        #self.handle = self.listen_state("input_boolean.test_alert",self.change,new = "on", duration = 10)
+        self.listen_state("input_boolean.test_alert",self.change,new = "on", duration = 10)
         self.log('Initialisation Test..', log="test_log")
+
 
     def change(self, entity, attribute, old, new, kwargs):
         self.log(f'Etat:{new}', log="test_log")

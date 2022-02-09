b71829bc42b6feda28d7e7ee7c861a5115b42cd2
root <root@homeassistant.local.hass.io>
2022-02-09 02:00:01 +0100

    config files on 09-02-2022 02:00:01

--- appdaemon/apps/surveille_activite copy.py
+++ appdaemon/apps/surveille_activite copy.py
@@ -2,12 +2,13 @@ import hassapi as hass
 
 class SurveilleActivite(hass.Hass):
     def initialize(self):
+        self.tempo = {}
         self.listen_state(self.change,self.args["entite"])
         nom_entité = self.get_state(self.args["entite"],attribute="entity_id")
         duree_tempo=int(self.args["tempo"])
         self.turn_on(self.args["activite_ok"])
-        self.tempo=self.run_in(self.notification, duree_tempo,entité=nom_entité,temps=duree_tempo)
-        self.log(f'Initialisation de: {nom_entité} pour {duree_tempo}s et {self.tempo}.', log="surveille_log")
+        self.tempo[nom_entité]=self.run_in(self.notification, duree_tempo,entité=nom_entité,temps=duree_tempo)
+        self.log(f'Initialisation de: {nom_entité}..pour {duree_tempo}s et {self.tempo[nom_entité]}.', log="surveille_log")
 
     def change(self, entity, attribute, old, new, kwargs):
         heure = str(self.time())[:8]
@@ -18,13 +19,13 @@ class SurveilleActivite(hass.Hass):
         # Mise à on de Com_Ok
         self.turn_on(self.args["activite_ok"])
         self.log(f'Nouvelle valeur de {entity}: {nouvelle_valeur}-Tempo={duree_tempo}', log="surveille_log")
-        cle_tempo = self.tempo
+        cle_tempo = self.tempo[nom_entité]
         if cle_tempo != None:
-            self.tempo = self.cancel_timer(cle_tempo) 
+            self.tempo[nom_entité] = self.cancel_timer(cle_tempo) 
             #self.log(f'Info tempo: {self.info_timer(cle_tempo)}', log="surveille_log")
             #self.log(f'Fin tempo {cle_tempo}', log="surveille_log")
 
-        self.tempo = self.run_in(self.notification, duree_tempo,entité=nom_entité,temps=duree_tempo)
+        self.tempo[nom_entité] = self.run_in(self.notification, duree_tempo,entité=nom_entité,temps=duree_tempo)
 
     def notification(self, kwargs):
         heure = str(self.time())[:8]
@@ -33,5 +34,5 @@ class SurveilleActivite(hass.Hass):
         # Mise à off de Com_Ok
         self.turn_off(self.args["activite_ok"])
         self.log(f'Alerte! {nom_entité} est out depuis {duree_temps} sec.', log="surveille_log")
-        self.call_service('notify/telegram', message=format(heure)+"Alerte!"+ format(nom_entité)+"est out depuis: "+format(duree_temps)+" sec.")
-        self.call_service('persistent_notification/create', message=format(heure)+"Alerte!"+ format(nom_entité)+"est out depuis: "+format(duree_temps)+" sec.")        
+        #self.call_service('notify/telegram', message=format(heure)+"Alerte!"+ format(nom_entité)+"est out depuis: "+format(duree_temps)+" sec.")
+        #self.call_service('persistent_notification/create', message=format(heure)+"Alerte!"+ format(nom_entité)+"est out depuis: "+format(duree_temps)+" sec.")        
--- appdaemon/apps/test.py
+++ appdaemon/apps/test.py
@@ -1,37 +1,30 @@
 import hassapi as hass
-import datetime
-from datetime import datetime, timedelta
-import subprocess
 
 class Test(hass.Hass):
     def initialize(self):
-        self.mon_entité = {}
-        group = self.get_state(self.args["groupe"], attribute = "all")
-        # Recupere les entités du groupe
-        entités = group["attributes"]["entity_id"]
         duree_tempo=int(self.args["tempo"])
+        name_entité = self.get_state(self.args["entite"],attribute="friendly_name")
+        self.mon_entité = self.get_entity(self.args["entite"])
+        self.mon_entité.listen_state(self.change,attribute="all", duration = duree_tempo,nom=name_entité)
+        self.tempo=self.run_in(self.notification, duree_tempo,entité=name_entité,temps=duree_tempo)
+        self.log(f'Surveillance de: {name_entité}', log="test_log")
 
-        for entité in entités:
-            self.log('Surveillance de: ' + str(entité), log="test_log")
-            # init timer dictionary, each entry represents a window (name->timer) 
-            name_entité = self.get_state(entité,attribute="entity_id")
-            self.mon_entité[entité] = self.get_entity(entité)
-            self.mon_entité[entité].listen_state(self.change_off,new = "off", duration = 10,nom=name_entité)
-            self.mon_entité[entité].listen_state(self.change_on,new = "on", duration = 1,nom=name_entité)
-
-#            self.my_entity = self.get_entity("input_boolean.test_alert")
-#            self.my_entity.listen_state(self.change_off,new = "off", duration = 10)
-#            self.my_entity.listen_state(self.change_on,new = "on", duration = 10)
-#            #self.listen_state("input_boolean.test_alert",self.change,new = "on", duration = 10)
-#            self.log('Initialisation Test..', log="test_log")
-
-    def change_off(self, entity, attribute, old, new, kwargs):
+    def change(self, entity, attribute, old, new, kwargs):
+        heure = str(self.time())[:8]
         nom_entité = kwargs["nom"]
-        self.log(f'Etat:{nom_entité}={new}', log="test_log")
-        
-    def change_on(self, entity, attribute, old, new, kwargs):
-        nom_entité = kwargs["nom"]
-        returncode = subprocess.run("/config/gitpush.sh",shell=True)
-        self.log(str(returncode), log="test_log")
-        self.log(f'Etat:{nom_entité}={new}', log="test_log")
-        
+        #self.log(f'Attention! Etat: {nom_entité} -> {new}/{flag[entity]}', log="test_log")
+        self.log(f'Changement Etat:{nom_entité} old= {old} new= {new}', log="test_log")
+        message_notification= format(heure)+":"+" Attention: "+ format(nom_entité)+" sur OFF"
+        self.tempo = self.cancel_timer
+        self.tempo=self.run_in(self.notification, duree_tempo,entité=name_entité,temps=duree_tempo)
+#        self.call_service('notify/telegram', message=message_notification)
+#        self.call_service('persistent_notification/create', message=message_notification)     
+
+    def notification(self, kwargs):
+        heure = str(self.time())[:8]
+        nom_entité = kwargs["entité"]
+        #self.log(f'Attention! Etat: {nom_entité} -> {new}/{flag[entity]}', log="test_log")
+        self.log(f'Etat:{nom_entité} old= {old} new= {new}', log="test_log")
+        message_notification= format(heure)+":"+" Attention: "+ format(nom_entité)+" sur OFF"
+#        self.call_service('notify/telegram', message=message_notification)
+#        self.call_service('persistent_notification/create', message=message_notification)     

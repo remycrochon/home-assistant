26b74f74143483a291e137fd78f5f66f556adf6a
root <root@core-ssh.local.hass.io>
2022-02-12 15:48:12 +0100

    config files on 12-02-2022 15:48:12

--- appdaemon/apps/alerte_fin_certificats.py
+++ appdaemon/apps/alerte_fin_certificats.py
@@ -11,7 +11,19 @@ class AlerteFinCertificats(hass.Hass):
         heure = str(self.time())[:8]
         nom_entité =  self.friendly_name(entity)
         s_bas= int(self.args["seuil_bas"])
-        if int(new) < int(self.args["seuil_bas"]):
+                
+        # Vérifie si le certificat est valide
+        validité = self.get_state(entity, "is_valid")
+        if validité == False:
+            self.log(f'Attention: Le certificat <{nom_entité}> est invalide', log="fin_certificats_log")
+            message_notification= format(heure)+": Attention: Le certificat <"+ format(nom_entité)+"> est invalide."
+            self.call_service('notify/telegram', message=message_notification)
+            self.call_service('persistent_notification/create', message=message_notification)
+        if validité != False or validité != True:
+            self.log(f'Entite <{nom_entité}> n est pas un certificat', log="fin_certificats_log")
+
+        # Vérifie si le nombre de jours est inférieur au seuil bas
+        if (validité == False or validité == True) and int(new) < int(self.args["seuil_bas"]):
             self.log(f'Attention! Le certificat <{nom_entité}> se termine dans {int(new)} J', log="fin_certificats_log")
             # Formatage du message de notification
             message_notification= format(heure)+": Attention: Fin du certificat <"+ format(nom_entité)+"> dans "+format(int(new))+" J."

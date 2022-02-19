94d59f1f568696d9378abaca01dc157cb27557aa
remycrochon <crochonremy@gmail.com>
2022-02-19 18:35:59 +0100

    config files on 19-02-2022 18:35:59

--- appdaemon/apps/alerte_fin_certificats.py
+++ appdaemon/apps/alerte_fin_certificats.py
@@ -1,33 +1,59 @@
 import hassapi as hass
+import datetime
+from datetime import datetime
+from datetime import timedelta
+import time
+
+
+JOURNAL=2
 
 class AlerteFinCertificats(hass.Hass):
     def initialize(self):
         if "certif" in self.args:
             for certif in self.split_device_list(self.args["certif"]):
                 self.listen_state(self.state_change, certif)
-                self.log(f'Surveillance de : {certif}',log="fin_certificats_log")
+                self.notification('Surveillance de:'+certif,2,"")
 
     def state_change(self, entity, attribute, old, new, kwargs):
         heure = str(self.time())[:8]
         nom_entité =  self.friendly_name(entity)
         s_bas= int(self.args["seuil_bas"])
-                
+        self.notification("New= "+new,2,"")
+        validité = self.get_state(entity,attribute="is_valid")
         # Vérifie si le certificat est valide
-        validité = self.get_state(entity, "is_valid")
-        if validité == False:
-            self.log(f'Attention: Le certificat <{nom_entité}> est invalide', log="fin_certificats_log")
-            message_notification= format(heure)+": Attention: Le certificat <"+ format(nom_entité)+"> est invalide."
-            self.call_service('notify/telegram', message=message_notification)
-            self.call_service('persistent_notification/create', message=message_notification)
-        if validité != False or validité != True:
-            self.log(f'Entite <{nom_entité}> n est pas un certificat', log="fin_certificats_log")
-
-        # Vérifie si le nombre de jours est inférieur au seuil bas
-        if (validité == False or validité == True) and int(new) < int(self.args["seuil_bas"]):
-            self.log(f'Attention! Le certificat <{nom_entité}> se termine dans {int(new)} J', log="fin_certificats_log")
-            # Formatage du message de notification
-            message_notification= format(heure)+": Attention: Fin du certificat <"+ format(nom_entité)+"> dans "+format(int(new))+" J."
-            self.call_service('notify/telegram', message=message_notification)
-            self.call_service('persistent_notification/create', message=message_notification)
-
+        if new !="unknown" or validité == True:
+            ce_jour=datetime.strptime(time.strftime('%Y:%m:%d', time.localtime()),'%Y:%m:%d')
+            self.notification("ce Jour:" + str(ce_jour),2,"")
+            date_de_fin=datetime.strptime(new[:10],'%Y-%m-%d')
+            self.notification("Date de Fin:" + str(date_de_fin),2,"")
+            if date_de_fin<ce_jour:
+                nb_jour=(date_de_fin-ce_jour).days
+                self.notification("nb Jour negatifs:" + str(nb_jour),2,"")
+            else:
+                nb_jour=(date_de_fin-ce_jour).days
+                self.notification("nb Jour:" + str(nb_jour),2,"")
+                # Vérifie si le nombre de jours est inférieur au seuil bas
+                if nb_jour < s_bas:
+                    message_notification= "Attention: Fin du certificat <"+ format(nom_entité)+"> dans "+ format(nb_jour)+" J."
+                    self.notification(message_notification,0,"teleg")
+                else:
+                    message_notification= "Le certificat <"+ format(nom_entité)+"> est encore valable "+ format(nb_jour)+" J."
+                    self.notification(message_notification,0,"teleg")
+        else:
+            message_notification= format(heure)+": Attention: Le certificat <"+ format(nom_entité)+"> n'est plus valide."
+            self.notification(message_notification,0,"teleg")
+            
+    # Fonction Notification
+    # message =  Texte à afficher
+    # niveau = niveau de journalisation 0,1,2
+    # si notif == "teleg" on notifie aussi sur Télégram
+    def notification(self,texte_message,niveau,notif):
+        global JOURNAL
+        heure = str(self.time())[:8]
+        if niveau <= JOURNAL:
+            message_notification= format(heure)+": "+ texte_message
+            self.log(message_notification, log="fin_certificats_log")
+            if notif=="teleg":
+                self.call_service('notify/telegram', message=message_notification)
+                self.call_service('persistent_notification/create', message=message_notification)
     
--- appdaemon/apps/test.py
+++ appdaemon/apps/test.py
@@ -1,5 +1,8 @@
 import hassapi as hass
-
+#import datetime
+from datetime import datetime
+from datetime import timedelta
+import time
 JOURNAL=2
 
 class Test(hass.Hass):
@@ -7,42 +10,49 @@ class Test(hass.Hass):
         if "certif" in self.args:
             for certif in self.split_device_list(self.args["certif"]):
                 self.listen_state(self.state_change, certif)
-                self.notification('Surveillance de:'+certif,2)
-                
+                self.notification('Surveillance de:'+certif,2,"")
 
     def state_change(self, entity, attribute, old, new, kwargs):
         heure = str(self.time())[:8]
         nom_entité =  self.friendly_name(entity)
         s_bas= int(self.args["seuil_bas"])
-        date_fin = new
-        self.notification("Date de Fin:" + str(date_fin),2)
-        validite = self.get_state(entity,attribute="is_valid")
-        self.notification("Validité:" + validite,2)
+        self.notification("New= "+new,2,"")
+        validité = self.get_state(entity,attribute="is_valid")
         # Vérifie si le certificat est valide
-        validité = self.get_state(entity, "is_valid")
-        if validité == False:
-            message_notification= format(heure)+": Attention: Le certificat <"+ format(nom_entité)+"> est invalide."
-            self.notification(message_notification,2)
+        if new !="unknown" or validité == True:
+            ce_jour=datetime.strptime(time.strftime('%Y:%m:%d', time.localtime()),'%Y:%m:%d')
+            self.notification("ce Jour:" + str(ce_jour),2,"")
+            date_de_fin=datetime.strptime(new[:10],'%Y-%m-%d')
+            self.notification("Date de Fin:" + str(date_de_fin),2,"")
+            if date_de_fin<ce_jour:
+                nb_jour=(date_de_fin-ce_jour).days
+                self.notification("nb Jour negatifs:" + str(nb_jour),2,"")
+            else:
+                nb_jour=(date_de_fin-ce_jour).days
+                self.notification("nb Jour:" + str(nb_jour),2,"")
+                # Vérifie si le nombre de jours est inférieur au seuil bas
+                if nb_jour < s_bas:
+                    message_notification= "Attention: Fin du certificat <"+ format(nom_entité)+"> dans "+ format(nb_jour)+" J."
+                    self.notification(message_notification,0,"teleg")
+                else:
+                    message_notification= "Le certificat <"+ format(nom_entité)+"> est encore valable "+ format(nb_jour)+" J."
+                    self.notification(message_notification,0,"teleg")
+        else:
+            message_notification= format(heure)+": Attention: Le certificat <"+ format(nom_entité)+"> n'est plus valide."
+            self.notification(message_notification,0,"teleg")
             
-        if validité != False or validité != True:
-            self.notification('Entite <'+nom_entité+'> n est pas un certificat',2)
-
-        # Vérifie si le nombre de jours est inférieur au seuil bas
-        if (validité == False or validité == True) and int(new) < int(self.args["seuil_bas"]):
-            message_notification= format(heure)+": Attention: Fin du certificat <"+ format(nom_entité)+"> dans "+format(int(new))+" J."
-            self.notification(message_notification,2)
-            
-
     # Fonction Notification
     # message =  Texte à afficher
     # niveau = niveau de journalisation 0,1,2
-    def notification(self,message,niveau):
+    # si notif == "teleg" on notifie aussi sur Télégram
+    def notification(self,texte_message,niveau,notif):
         global JOURNAL
         heure = str(self.time())[:8]
         if niveau <= JOURNAL:
-            message_notification= format(heure)+": "+ message
+            message_notification= format(heure)+": "+ texte_message
             self.log(message_notification, log="test_log")
-            #self.call_service('notify/telegram', message=message_notification)
-            #self.call_service('persistent_notification/create', message=message_notification)
+            if notif=="teleg":
+                self.call_service('notify/telegram', message=message_notification)
+                self.call_service('persistent_notification/create', message=message_notification)
 
     

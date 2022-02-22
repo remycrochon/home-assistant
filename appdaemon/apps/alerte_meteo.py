83a60b617b6f4e2defbfaf10b87dc7d83fb8c81e
remycrochon <crochonremy@gmail.com>
2022-02-22 02:00:00 +0100

    config files on 22-02-2022 02:00:00

--- appdaemon/apps/alerte_meteo.py
+++ appdaemon/apps/alerte_meteo.py
@@ -1,8 +1,10 @@
 import hassapi as hass
 import datetime
 from datetime import datetime
+from datetime import timedelta
+import time
 
-# Niveau de JOURNALISATION (log): 0=rien ou 1 =info ou 2=debug 
+# Niveau de JOURNALISATION (log): 0=aucune notif ou 1 =info ou 2=debug 
 JOURNAL=2 
 h_forcast={}
 FLAG=0
@@ -16,51 +18,49 @@ class AlerteMeteo(hass.Hass):
         forcast = self.get_state(self.args["entité"],attribute="forecast_time_ref")
         self.log(f"Forecast: {forcast}", log="test_log")
         
-        infov=self.convert_utc(forcast)
-        infov=self.time()
-        infov=self.get_timezone()
-        infov=self.get_tz_offset()
-        self.log(f"infov: {infov}", log="test_log")
-        
         # Ecoute une alerte méteo
         self.listen_state(self.change_alerte, self.args["alerte"])
-        #alerte_w = self.get_state(self.args["alerte"],attribute="state")
-        #self.log(f"Alerte Meteo: {alerte_w}", log="test_log")
-
-        #self.notifie_pluie(kwargs=forcast)
 
     def change_pluie(self, entity, attribute, old, new, kwargs):
         global FLAG
         message_notification= "New: "+new+" / Old: "+old+" / Flag="+ str(FLAG)
         self.notification(message_notification,2,"")
         dic_forcast = self.get_state(self.args["entité"],attribute="1_hour_forecast")
-        h_forcast[0] = dic_forcast['0 min']
-        h_forcast[1] = dic_forcast['5 min']
-        h_forcast[2] = dic_forcast['10 min']
-        h_forcast[3] = dic_forcast['15 min']
-        h_forcast[4] = dic_forcast['20 min']
-        h_forcast[5] = dic_forcast['25 min']
-        h_forcast[6] = dic_forcast['35 min']
-        h_forcast[7] = dic_forcast['45 min']
-        h_forcast[8] = dic_forcast['55 min']
-        for h in range(8):
-            message_notification= "h"+str(h)+"_Forecast:"+h_forcast[h]
-            self.notification(message_notification,2,"")
+        # Calcul décalage UTC <-> heure local
+        decalage_utc=self.get_tz_offset()/60
+        self.notification("Decalage UTC: "+str(decalage_utc),2,"")
+        # Affiche l'heure locale
+        #h_locale=time.strftime('%H:%M:%S', time.localtime())[:5]
+        #self.notification("Heure Locale:" + str(h_locale),2,"")
 
-        etat = self.get_state(self.args["entité"],attribute="state")
-        #self.log(f'Etat: {etat}', log="test_log")
-        h_pluie= etat[11:16] # Extrait l'heure de la chaine de caracteres
+        if new!="unknown":
+            # Ajoute le décalage UTC/Heure locale à l'heure transmise par Meteo France
+            h_pluie=datetime.strftime(datetime.strptime(new[11:19],'%H:%M:%S')+timedelta(hours=decalage_utc),"%H:%M:%S")
+            h_forcast[0] = dic_forcast['0 min']
+            h_forcast[1] = dic_forcast['5 min']
+            h_forcast[2] = dic_forcast['10 min']
+            h_forcast[3] = dic_forcast['15 min']
+            h_forcast[4] = dic_forcast['20 min']
+            h_forcast[5] = dic_forcast['25 min']
+            h_forcast[6] = dic_forcast['35 min']
+            h_forcast[7] = dic_forcast['45 min']
+            h_forcast[8] = dic_forcast['55 min']
+            for h in range(8):
+                message_notification= "h"+str(h)+"_Forecast:"+h_forcast[h]
+                self.notification(message_notification,2,"")
+                
+            if FLAG==0: # Permet d'afficher le message une seule fois
+                message_notification= " La pluie est attendue a "+ format(h_pluie)
+                self.notification(message_notification,1,"teleg")
+                FLAG = 1
 
-        if new!="unknown" and FLAG==0:
-            message_notification= ": La pluie est attendue a "+ format(h_pluie)
-            self.notification(message_notification,0,"teleg")
-            FLAG = 1
-
-        if new=="unknown" or h_forcast[0] != "Temps sec":
-            message_notification= ": Plus de pluie attendue."
+        if new=="unknown" and h_forcast[0] == "Temps sec":
+            message_notification= " Plus de pluie attendue."
             FLAG=0
-            self.notification(message_notification,0,"teleg")
-
+            self.notification(message_notification,1,"teleg")
+        
+        message_notification= "Flag="+ str(FLAG)
+        self.notification(message_notification,2,"")
 
     def change_alerte(self, entity, attribute, old, new, kwargs):
         heure = str(self.time())[:8]
@@ -73,34 +73,34 @@ class AlerteMeteo(hass.Hass):
         Vent_violent=self.get_state(self.args["alerte"],attribute="Vent violent")
         attribution= self.get_state(self.args["alerte"],attribute="attribution")
         
-        if alerte_w != 'Vert' or alerte_w != 'unavailable':
-            if Inondation != 'Vert':
-                message_notification= ": Alerte Innondation :"+ Inondation
-                self.notification(message_notification,2,"")
+        if alerte_w != 'Vert':
+            if alerte_w != 'unavailable':
+                if Inondation != 'Vert':
+                    message_notification= ": Alerte Innondation :"+ Inondation
+                    self.notification(message_notification,1,"teleg")
 
-            if Grand_Froid != 'Vert':
-                message_notification= ": Alerte Grand-froid :"+Grand_Froid
-                self.notification(message_notification,2,"")
+                if Grand_Froid != 'Vert':
+                    message_notification= ": Alerte Grand-froid :"+Grand_Froid
+                    self.notification(message_notification,1,"teleg")
 
-            if Neige_Verglas != 'Vert':
-                message_notification= ": Alerte Neige Verglas :"+Neige_Verglas
-                self.notification(message_notification,2,"")
+                if Neige_Verglas != 'Vert':
+                    message_notification= ": Alerte Neige Verglas :"+Neige_Verglas
+                    self.notification(message_notification,1,"teleg")
 
-            if Orages != 'Vert':
-                message_notification= ": Alerte Orages :"+Orages
-                self.notification(message_notification,2,"")
+                if Orages != 'Vert':
+                    message_notification= ": Alerte Orages :"+Orages
+                    self.notification(message_notification,1,"teleg")
 
-            if Pluie_inondation != 'Vert':
-                message_notification= ": Alerte Pluie Inondation :"+Pluie_inondation
-                self.notification(message_notification,2,"")
+                if Pluie_inondation != 'Vert':
+                    message_notification= ": Alerte Pluie Inondation :"+Pluie_inondation
+                    self.notification(message_notification,1,"teleg")
 
-            if Vent_violent != 'Vert':
-                message_notification= ": Alerte vents Violents :"+Vent_violent
-                self.notification(message_notification,2,"")
-
-        if alerte_w == 'unavailable':
+                if Vent_violent != 'Vert':
+                    message_notification= ": Alerte vents Violents :"+Vent_violent
+                self.notification(message_notification,1,"teleg")
+            else:
                 message_notification= ": Fin Alerte Météo "
-                self.notification(message_notification,2,"")
+                self.notification(message_notification,1,"teleg")
                 
     # Fonction Notification
     # message =  Texte à afficher
@@ -110,7 +110,7 @@ class AlerteMeteo(hass.Hass):
         global JOURNAL
         heure = str(self.time())[:8]
         if niveau <= JOURNAL:
-            message_notification= format(heure)+": "+ texte_message
+            message_notification= format(heure)+"-"+ texte_message
             self.log(message_notification, log="test_log")
             if notif=="teleg":
                 self.call_service('notify/telegram', message=message_notification)

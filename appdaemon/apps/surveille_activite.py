8ba9286b62c6db0063f08362d04b75d37562a0ac
remycrochon <crochonremy@gmail.com>
2022-03-05 02:00:00 +0100

    config files on 05-03-2022 02:00:00

--- appdaemon/apps/surveille_activite.py
+++ appdaemon/apps/surveille_activite.py
@@ -33,6 +33,6 @@ class SurveilleActivite(hass.Hass):
         # Mise à off de Com_Ok
         self.turn_off(self.args["activite_ok"])
         self.log(f'Alerte! {nom_entité} est out depuis {duree_temps} sec.', log="surveille_log")
-        message_notification=format(heure)+"Alerte!"+ format(nom_entité)+"est out depuis: "+format(duree_temps)+" sec."
-        self.call_service('notify/telegram', message_notification)
-        self.call_service('dwains_dashboard/notification_create', message=message_notification)
+        #message_notification=format(heure)+"Alerte!"+ format(nom_entité)+"est out depuis: "+format(duree_temps)+" sec."
+        #self.call_service('notify/telegram', message_notification)
+        #self.call_service('dwains_dashboard/notification_create', message=message_notification)

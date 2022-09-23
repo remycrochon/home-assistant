ab0209d0cc54a4bcfb86ef57a6daff9f174db622
remycrochon <crochonremy@gmail.com>
2022-09-23 02:00:00 +0200

    config files on 23-09-2022 02:00:00

--- custom_components/husqvarna_automower/const.py
+++ custom_components/husqvarna_automower/const.py
@@ -5,9 +5,9 @@ from homeassistant.const import Platform
 NAME = "husqvarna_automower"
 DOMAIN = "husqvarna_automower"
 DOMAIN_DATA = f"{DOMAIN}_data"
-INTEGRATION_VERSION = "2022.8.1"
+INTEGRATION_VERSION = "2022.9.0"
 ISSUE_URL = "https://github.com/Thomas55555/husqvarna_automower"
-HUSQVARNA_URL = "https://developer.husqvarnagroup.cloud/applications"
+HUSQVARNA_URL = "https://developer.husqvarnagroup.cloud/login"
 OAUTH2_AUTHORIZE = "https://api.authentication.husqvarnagroup.dev/v1/oauth2/authorize"
 OAUTH2_TOKEN = "https://api.authentication.husqvarnagroup.dev/v1/oauth2/token"
 

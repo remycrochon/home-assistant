8b8ac33ef429469bf5c371cce71a5aeda9b1374a
remycrochon <crochonremy@gmail.com>
2023-07-19 02:00:01 +0200

    config files on 19-07-2023 02:00:01

--- custom_components/nodered/const.py
+++ custom_components/nodered/const.py
@@ -2,7 +2,7 @@
 # Base component constants
 DOMAIN = "nodered"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "1.2.0"
+VERSION = "2.1.0"
 
 ISSUE_URL = "https://github.com/zachowj/hass-node-red/issues"
 
@@ -21,17 +21,24 @@ CONF_ENTITY_PICTURE = "entity_picture"
 CONF_LAST_RESET = "last_reset"
 CONF_NAME = "name"
 CONF_NODE_ID = "node_id"
+CONF_NUMBER = "number"
+CONF_OPTIONS = "options"
 CONF_OUTPUT_PATH = "output_path"
 CONF_PAYLOAD = "payload"
 CONF_REMOVE = "remove"
+CONF_SELECT = "select"
 CONF_SENSOR = "sensor"
 CONF_SERVER_ID = "server_id"
 CONF_SKIP_CONDITION = "skip_condition"
 CONF_STATE_CLASS = "state_class"
 CONF_SUB_TYPE = "sub_type"
 CONF_SWITCH = "switch"
+CONF_TEXT = "text"
+CONF_TIME = "time"
 CONF_TRIGGER_ENTITY_ID = "trigger_entity_id"
 CONF_VERSION = "version"
+
+EVENT_VALUE_CHANGE = "value_change"
 
 NODERED_DISCOVERY = "nodered_discovery"
 NODERED_DISCOVERY_NEW = "nodered_discovery_new_{}"
@@ -43,7 +50,11 @@ SERVICE_TRIGGER = "trigger"
 
 # Defaults
 NAME = "Node-RED Companion"
+NUMBER_ICON = "mdi:numeric"
 SWITCH_ICON = "mdi:electric-switch-closed"
+SELECT_ICON = "mdi:format-list-bulleted"
+TEXT_ICON = "mdi:form-textbox"
+TIME_ICON = "mdi:clock-time-three"
 
 STARTUP_MESSAGE = f"""
 -------------------------------------------------------------------

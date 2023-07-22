51c988d1458fb7869e0375513c80c650b8e75763
remycrochon <crochonremy@gmail.com>
2023-07-22 10:38:15 +0200

    config files on 22-07-2023 10:38:15

---
+++ custom_components/daikin_residential/const.py
@@ -0,0 +1,207 @@
+"""Constants for Daikin Residential Controller."""
+
+from homeassistant.const import (
+    CONF_DEVICE_CLASS,
+    CONF_TOKEN,
+    CONF_ICON,
+    CONF_NAME,
+    CONF_TYPE,
+    CONF_UNIT_OF_MEASUREMENT,
+    DEVICE_CLASS_TEMPERATURE,
+    DEVICE_CLASS_HUMIDITY,
+    DEVICE_CLASS_ENERGY,
+    DEVICE_CLASS_SIGNAL_STRENGTH,
+    ENERGY_KILO_WATT_HOUR,
+    TEMP_CELSIUS,
+    PERCENTAGE,
+    SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+)
+
+DOMAIN = "daikin_residential"
+
+CONF_TOKENSET = CONF_TOKEN + "set"
+
+DAIKIN_DATA = "daikin_data"
+DAIKIN_API = "daikin_api"
+DAIKIN_DEVICES = "daikin_devices"
+DAIKIN_DISCOVERY_NEW = "daikin_discovery_new_{}"
+
+ATTR_ON_OFF = "on_off"
+ATTR_PRESET_MODE = "preset_mode"
+ATTR_OPERATION_MODE = "operation_mode"
+ATTR_TEMPERATURE = "temperature"
+ATTR_TARGET_TEMPERATURE = "target_temperature"
+ATTR_INSIDE_TEMPERATURE = "inside_temperature"
+ATTR_OUTSIDE_TEMPERATURE = "outside_temperature"
+ATTR_WIFI_STRENGTH = "wifi_strength"
+ATTR_WIFI_SSID = "wifi_ssid"
+ATTR_LOCAL_SSID = "local_ssid"
+ATTR_MAC_ADDRESS = "mac_address"
+ATTR_SERIAL_NUMBER = "serial_number"
+ATTR_ENERGY_CONSUMPTION = "energy_consumption"
+ATTR_ROOM_HUMIDITY = "room_humidity"
+ATTR_TARGET_HUMIDITY = "target_humidity"
+ATTR_FAN_MODE = "fan_mode"
+ATTR_FAN_SPEED = "fan_speed"
+ATTR_HSWING_MODE = "hswing_mode"
+ATTR_VSWING_MODE = "vswing_mode"
+ATTR_SWING_AUTO = "auto"
+ATTR_SWING_SWING = "swing"
+ATTR_SWING_STOP = "stop"
+ATTR_COOL_ENERGY = "cool_energy"
+ATTR_HEAT_ENERGY = "heat_energy"
+
+MP_CLIMATE = "climateControl"
+MP_GATEWAY = "gateway"
+DP_ON_OFF = "onOffMode"
+DP_OPERATION_MODE = "operationMode"
+DP_SENSORS = "sensoryData"
+DP_TEMPERATURE = "temperatureControl"
+DP_FAN = "fanControl"
+DP_CONSUMPTION = "consumptionData"
+DP_WIFI_STRENGTH = "wifiConnectionStrength"
+DP_WIFI_SSID = "wifiConnectionSSID"
+DP_LOCAL_SSID = "ssid"
+DP_MAC_ADDRESS = "macAddress"
+DP_SERIAL_NUMBER = "serialNumber"
+
+DAIKIN_CMD_SETS = {
+    ATTR_ON_OFF: [MP_CLIMATE, DP_ON_OFF, ""],
+    ATTR_PRESET_MODE: [MP_CLIMATE, "", ""],
+    ATTR_OPERATION_MODE: [MP_CLIMATE, DP_OPERATION_MODE, ""],
+    ATTR_OUTSIDE_TEMPERATURE: [MP_CLIMATE, DP_SENSORS, "/outdoorTemperature"],
+    ATTR_INSIDE_TEMPERATURE: [MP_CLIMATE, DP_SENSORS, "/roomTemperature"],
+    ATTR_ROOM_HUMIDITY: [MP_CLIMATE, DP_SENSORS, "/roomHumidity"],
+    ATTR_TARGET_TEMPERATURE: [
+        MP_CLIMATE,
+        DP_TEMPERATURE,
+        "/operationModes/%operationMode%/setpoints/roomTemperature",
+    ],
+    ATTR_FAN_MODE: [
+        MP_CLIMATE,
+        DP_FAN,
+        "/operationModes/%operationMode%/fanSpeed/currentMode",
+    ],
+    ATTR_FAN_SPEED: [
+        MP_CLIMATE,
+        DP_FAN,
+        "/operationModes/%operationMode%/fanSpeed/modes/fixed",
+    ],
+    ATTR_HSWING_MODE: [
+        MP_CLIMATE,
+        DP_FAN,
+        "/operationModes/%operationMode%/fanDirection/horizontal/currentMode",
+    ],
+    ATTR_VSWING_MODE: [
+        MP_CLIMATE,
+        DP_FAN,
+        "/operationModes/%operationMode%/fanDirection/vertical/currentMode",
+    ],
+    ATTR_ENERGY_CONSUMPTION: [MP_CLIMATE, DP_CONSUMPTION, "/electrical"],
+    ATTR_WIFI_STRENGTH: [MP_GATEWAY, DP_WIFI_STRENGTH, ""],
+    ATTR_WIFI_SSID: [MP_GATEWAY, DP_WIFI_SSID, ""],
+    ATTR_LOCAL_SSID: [MP_GATEWAY, DP_LOCAL_SSID, ""],
+    ATTR_MAC_ADDRESS: [MP_GATEWAY, DP_MAC_ADDRESS, ""],
+    ATTR_SERIAL_NUMBER: [MP_GATEWAY, DP_SERIAL_NUMBER, ""],
+}
+
+ATTR_STATE_ON = "on"
+ATTR_STATE_OFF = "off"
+
+FAN_FIXED = "fixed"
+FAN_QUIET = "Silence"
+
+SWING_OFF = "Off"
+SWING_BOTH = "3D"
+SWING_VERTICAL = "Vertical"
+SWING_HORIZONTAL = "Horizontal"
+
+PRESET_STREAMER = "streamer"
+
+SENSOR_TYPE_TEMPERATURE = "temperature"
+SENSOR_TYPE_HUMIDITY = "humidity"
+SENSOR_TYPE_GATEWAY_DIAGNOSTIC = "gateway_diagnostic"
+SENSOR_TYPE_POWER = "power"
+SENSOR_TYPE_ENERGY = "energy"
+SENSOR_PERIOD_DAILY = "d"
+SENSOR_PERIOD_WEEKLY = "w"
+SENSOR_PERIOD_YEARLY = "m"
+SENSOR_PERIODS = {
+    SENSOR_PERIOD_DAILY: "Daily",
+    SENSOR_PERIOD_WEEKLY: "Weekly",
+    SENSOR_PERIOD_YEARLY: "Yearly",
+}
+
+SENSOR_TYPES = {
+    ATTR_INSIDE_TEMPERATURE: {
+        CONF_NAME: "Inside Temperature",
+        CONF_TYPE: SENSOR_TYPE_TEMPERATURE,
+        CONF_DEVICE_CLASS: DEVICE_CLASS_TEMPERATURE,
+        CONF_UNIT_OF_MEASUREMENT: TEMP_CELSIUS,
+    },
+    ATTR_OUTSIDE_TEMPERATURE: {
+        CONF_NAME: "Outside Temperature",
+        CONF_TYPE: SENSOR_TYPE_TEMPERATURE,
+        CONF_DEVICE_CLASS: DEVICE_CLASS_TEMPERATURE,
+        CONF_UNIT_OF_MEASUREMENT: TEMP_CELSIUS,
+    },
+    ATTR_ROOM_HUMIDITY: {
+        CONF_NAME: "Room Humidity",
+        CONF_TYPE: SENSOR_TYPE_HUMIDITY,
+        CONF_DEVICE_CLASS: DEVICE_CLASS_HUMIDITY,
+        CONF_UNIT_OF_MEASUREMENT: PERCENTAGE,
+    },
+    ATTR_COOL_ENERGY: {
+        CONF_NAME: "Cool Energy Consumption",
+        CONF_TYPE: SENSOR_TYPE_ENERGY,
+        CONF_ICON: "mdi:snowflake",
+        CONF_DEVICE_CLASS: DEVICE_CLASS_ENERGY,
+        CONF_UNIT_OF_MEASUREMENT: ENERGY_KILO_WATT_HOUR,
+    },
+    ATTR_HEAT_ENERGY: {
+        CONF_NAME: "Heat Energy Consumption",
+        CONF_TYPE: SENSOR_TYPE_ENERGY,
+        CONF_ICON: "mdi:fire",
+        CONF_DEVICE_CLASS: DEVICE_CLASS_ENERGY,
+        CONF_UNIT_OF_MEASUREMENT: ENERGY_KILO_WATT_HOUR,
+    },
+    ATTR_WIFI_STRENGTH: {
+        CONF_NAME: "WiFi Strength",
+        CONF_TYPE: SENSOR_TYPE_GATEWAY_DIAGNOSTIC,
+        CONF_DEVICE_CLASS: DEVICE_CLASS_SIGNAL_STRENGTH,
+        CONF_UNIT_OF_MEASUREMENT: SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+    },
+    ATTR_WIFI_SSID: {
+        CONF_NAME: "WiFi SSID",
+        CONF_TYPE: SENSOR_TYPE_GATEWAY_DIAGNOSTIC,
+        CONF_ICON: "mdi:access-point-network",
+        CONF_DEVICE_CLASS: None,
+        CONF_UNIT_OF_MEASUREMENT: None,
+    },
+    ATTR_LOCAL_SSID: {
+        CONF_NAME: "Internal SSID",
+        CONF_TYPE: SENSOR_TYPE_GATEWAY_DIAGNOSTIC,
+        CONF_DEVICE_CLASS: None,
+        CONF_UNIT_OF_MEASUREMENT: None,
+    },
+    ATTR_MAC_ADDRESS: {
+        CONF_NAME: "Mac Address",
+        CONF_TYPE: SENSOR_TYPE_GATEWAY_DIAGNOSTIC,
+        CONF_DEVICE_CLASS: None,
+        CONF_UNIT_OF_MEASUREMENT: None,
+    },
+    ATTR_SERIAL_NUMBER: {
+        CONF_NAME: "Serial Number",
+        CONF_TYPE: SENSOR_TYPE_GATEWAY_DIAGNOSTIC,
+        CONF_ICON: "mdi:numeric",
+        CONF_DEVICE_CLASS: None,
+        CONF_UNIT_OF_MEASUREMENT: None,
+    },
+}
+
+CONF_UUID = "uuid"
+
+KEY_MAC = "mac"
+KEY_IP = "ip"
+
+TIMEOUT = 60
---
+++ custom_components/daikin_residential/sensor.py
@@ -0,0 +1,243 @@
+"""Support for Daikin AC sensors."""
+import logging
+
+from homeassistant.const import (
+    CONF_DEVICE_CLASS,
+    CONF_ICON,
+    CONF_NAME,
+    CONF_TYPE,
+    CONF_UNIT_OF_MEASUREMENT,
+)
+from homeassistant.components.sensor import (
+    SensorEntity,
+    STATE_CLASS_MEASUREMENT,
+    STATE_CLASS_TOTAL_INCREASING,
+)
+
+from homeassistant.helpers.entity import EntityCategory
+
+from .daikin_base import Appliance
+
+from .const import (
+    DOMAIN as DAIKIN_DOMAIN,
+    DAIKIN_DEVICES,
+    ATTR_COOL_ENERGY,
+    ATTR_HEAT_ENERGY,
+    ATTR_INSIDE_TEMPERATURE,
+    ATTR_OUTSIDE_TEMPERATURE,
+    ATTR_ROOM_HUMIDITY,
+    ATTR_WIFI_STRENGTH,
+    ATTR_WIFI_SSID,
+    ATTR_LOCAL_SSID,
+    ATTR_MAC_ADDRESS,
+    ATTR_SERIAL_NUMBER,
+    SENSOR_TYPE_ENERGY,
+    SENSOR_TYPE_HUMIDITY,
+    SENSOR_TYPE_POWER,
+    SENSOR_TYPE_TEMPERATURE,
+    SENSOR_TYPE_GATEWAY_DIAGNOSTIC,
+    SENSOR_PERIODS,
+    SENSOR_TYPES,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(hass, async_add_entities):
+    """Old way of setting up the Daikin sensors.
+
+    Can only be called when a user accidentally mentions the platform in their
+    config. But even in that case it would have been ignored.
+    """
+
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up Daikin climate based on config_entry."""
+    sensors = []
+    for dev_id, device in hass.data[DAIKIN_DOMAIN][DAIKIN_DEVICES].items():
+        if device.support_inside_temperature:
+            _LOGGER.debug("device %s supports inside temperature", device.name)
+            sensor = DaikinSensor.factory(device, ATTR_INSIDE_TEMPERATURE)
+            sensors.append(sensor)
+        if device.support_outside_temperature:
+            _LOGGER.debug("device %s supports outside temperature", device.name)
+            sensor = DaikinSensor.factory(device, ATTR_OUTSIDE_TEMPERATURE)
+            sensors.append(sensor)
+        if device.support_room_humidity:
+            _LOGGER.debug("device %s supports room humidity", device.name)
+            sensor = DaikinSensor.factory(device, ATTR_ROOM_HUMIDITY)
+            sensors.append(sensor)
+        if device.support_energy_consumption:
+            _LOGGER.debug("device %s supports energy consumption", device.name)
+            for period in SENSOR_PERIODS:
+                sensor = DaikinSensor.factory(device, ATTR_COOL_ENERGY, period)
+                sensors.append(sensor)
+                sensor = DaikinSensor.factory(device, ATTR_HEAT_ENERGY, period)
+                sensors.append(sensor)
+        if device.support_wifi_strength:
+            _LOGGER.debug("device %s supports wifi signal strength", device.name)
+            sensor = DaikinSensor.factory(device, ATTR_WIFI_STRENGTH)
+            sensors.append(sensor)
+        if device.support_wifi_ssid:
+            _LOGGER.debug("device %s supports wifi ssid", device.name)
+            sensor = DaikinSensor.factory(device, ATTR_WIFI_SSID)
+            sensors.append(sensor)
+        if device.support_local_ssid:
+            _LOGGER.debug("device %s supports local ssid", device.name)
+            sensor = DaikinSensor.factory(device, ATTR_LOCAL_SSID)
+            sensors.append(sensor)
+        if device.support_mac_address:
+            _LOGGER.debug("device %s supports mac address", device.name)
+            sensor = DaikinSensor.factory(device, ATTR_MAC_ADDRESS)
+            sensors.append(sensor)
+        if device.support_serial_number:
+            _LOGGER.debug("device %s supports serial number", device.name)
+            sensor = DaikinSensor.factory(device, ATTR_SERIAL_NUMBER)
+            sensors.append(sensor)
+    async_add_entities(sensors)
+
+
+class DaikinSensor(SensorEntity):
+    """Representation of a Sensor."""
+
+    @staticmethod
+    def factory(device: Appliance, monitored_state: str, period=""):
+        """Initialize any DaikinSensor."""
+        cls = {
+            SENSOR_TYPE_TEMPERATURE: DaikinClimateSensor,
+            SENSOR_TYPE_HUMIDITY: DaikinClimateSensor,
+            SENSOR_TYPE_POWER: DaikinEnergySensor,
+            SENSOR_TYPE_ENERGY: DaikinEnergySensor,
+            SENSOR_TYPE_GATEWAY_DIAGNOSTIC: DaikinGatewaySensor,
+        }[SENSOR_TYPES[monitored_state][CONF_TYPE]]
+        return cls(device, monitored_state, period)
+
+    def __init__(self, device: Appliance, monitored_state: str, period="") -> None:
+        """Initialize the sensor."""
+        self._device = device
+        self._sensor = SENSOR_TYPES[monitored_state]
+        self._period = period
+        if period != "":
+            periodName = SENSOR_PERIODS[period]
+            self._name = f"{device.name} {periodName} {self._sensor[CONF_NAME]}"
+        else:
+            self._name = f"{device.name} {self._sensor[CONF_NAME]}"
+        self._device_attribute = monitored_state
+
+    @property
+    def available(self):
+        """Return the availability of the underlying device."""
+        return self._device.available
+
+    @property
+    def unique_id(self):
+        """Return a unique ID."""
+        devID = self._device.getId()
+        if self._period != "":
+            return f"{devID}_{self._device_attribute}_{self._period}"
+        return f"{devID}_{self._device_attribute}"
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        raise NotImplementedError
+
+    @property
+    def device_class(self):
+        """Return the class of this device."""
+        return self._sensor.get(CONF_DEVICE_CLASS)
+
+    @property
+    def icon(self):
+        """Return the icon of this device."""
+        return self._sensor.get(CONF_ICON)
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return self._sensor[CONF_UNIT_OF_MEASUREMENT]
+
+    async def async_update(self):
+        """Retrieve latest state."""
+        await self._device.api.async_update()
+
+    @property
+    def device_info(self):
+        """Return a device description for device registry."""
+        return self._device.device_info()
+
+
+class DaikinClimateSensor(DaikinSensor):
+    """Representation of a Climate Sensor."""
+
+    @property
+    def state(self):
+        """Return the internal state of the sensor."""
+        if self._device_attribute == ATTR_INSIDE_TEMPERATURE:
+            return self._device.inside_temperature
+        if self._device_attribute == ATTR_OUTSIDE_TEMPERATURE:
+            return self._device.outside_temperature
+        if self._device_attribute == ATTR_ROOM_HUMIDITY:
+            return self._device.room_humidity
+        return None
+
+    @property
+    def state_class(self):
+        return STATE_CLASS_MEASUREMENT
+
+
+class DaikinEnergySensor(DaikinSensor):
+    """Representation of a power/energy consumption sensor."""
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        if self._device_attribute == ATTR_COOL_ENERGY:
+            return round(self._device.energy_consumption("cooling", self._period), 3)
+        if self._device_attribute == ATTR_HEAT_ENERGY:
+            return round(self._device.energy_consumption("heating", self._period), 3)
+        return None
+
+    @property
+    def state_class(self):
+        return STATE_CLASS_TOTAL_INCREASING
+
+
+class DaikinGatewaySensor(DaikinSensor):
+    """Representation of a WiFi Sensor."""
+
+    # set default category for these entities
+    _attr_entity_category = EntityCategory.DIAGNOSTIC
+
+    @property
+    def state(self):
+        """Return the internal state of the sensor."""
+        if self._device_attribute == ATTR_WIFI_STRENGTH:
+            return self._device.wifi_strength
+        if self._device_attribute == ATTR_WIFI_SSID:
+            return self._device.wifi_ssid
+        if self._device_attribute == ATTR_LOCAL_SSID:
+            return self._device.local_ssid
+        if self._device_attribute == ATTR_MAC_ADDRESS:
+            return self._device.mac_address
+        if self._device_attribute == ATTR_SERIAL_NUMBER:
+            return self._device.serial_number
+        return None
+
+    @property
+    def state_class(self):
+        if self._device_attribute == ATTR_WIFI_STRENGTH:
+            return STATE_CLASS_MEASUREMENT
+        else:
+            return None
+
+    @property
+    def entity_registry_enabled_default(self):
+        # auto disable these entities when added for the first time
+        # except the wifi signal
+        return self._device_attribute == ATTR_WIFI_STRENGTH
